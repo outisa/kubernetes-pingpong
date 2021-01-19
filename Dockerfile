79822ff312bfd47d8fb9ddac082f7574f5f7f338
outisa <savolainen.outi@gmail.com>
2021-01-19 13:52:05 +0200

    Add persistent volume claime

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:alpine
 WORKDIR app
 COPY . .
 
-RUN npm ci
+RUN npm ci && mkdir files && cd files && touch pongs.txt && cd ..
 
 EXPOSE 3002
 
--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
+const directory = path.join('/', 'app', 'files')
+const pathToFile = path.join(directory, 'pongs.txt')
 let counter = 0;
+fs.writeFile(pathToFile, `Ping / Pongs: ${counter}`, (err) => { 
+  if (err) { 
+    console.log(err); 
+  }  
+})
 app.get('/pingpong', (request, response) =>  {
   response.send(`pong ${counter}`)
   counter = counter + 1
+  fs.writeFile(pathToFile, `Ping / Pongs: ${counter}`, (err) => { 
+    if (err) { 
+      console.log(err); 
+    }  
+  })
 })
 
 module.exports = app
