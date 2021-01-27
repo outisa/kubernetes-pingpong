629080f394786181d858d51c6d540a0b8146d799
outisa <savolainen.outi@gmail.com>
2021-01-27 12:01:48 +0200

    Modify postgres configuretion

--- queries.js
+++ queries.js
@@ -1,11 +1,5 @@
 const { Pool } = require('pg')
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
-})
+const pool = new Pool(`postgres://postgres${POSTGRES_PASSWORD}@postgres-svc.mainapp-namespace:5432/postgres`)
 
 pool.on('connect', () => {
   console.log('connected to the db')
